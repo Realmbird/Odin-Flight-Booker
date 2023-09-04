5d3fb4bc664c5234d149536daa463766b02c7844
Christopher Kinoshita <happychristopher777@gmail.com>
2023-09-04 08:07:59 -0700

    Finished adding Confirmation Emails

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,6 +1,7 @@
 class PassengerMailer < ApplicationMailer
+  default to: "happychristopher777@gmail.com" 
   def confirmation_email(passenger)
     @passenger = passenger
-    mail(to: @passenger.email, subject: "Welcome to My Awesome Site #{@passenger.name}")
+    mail(subject: "Welcome to My Awesome Site #{@passenger.name}")
   end
 end
